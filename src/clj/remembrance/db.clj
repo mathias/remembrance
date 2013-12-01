86001500643eb68da7757aba8372f59e284302bf
Matt Gauger <matt.gauger@gmail.com>
2013-11-30 18:31:46 -0600

    Create new articles with original_url
    - Don't recreate one that already exists based on original_url
    - Show an article

--- src/clj/remembrance/db.clj
+++ src/clj/remembrance/db.clj
@@ -8,8 +8,28 @@
 
 (def connection (d/connect db-uri))
 
-(defn info []
-  connection)
+(defn db []
+  (d/db connection))
+
+(def q
+  #(d/q % (db)))
+
+(defn ent [id]
+  (if-let [exists (ffirst (d/q '[:find ?eid :in $ ?eid :where [?eid]] (db) id))]
+    (d/entity (db) exists)
+    nil))
+
+(defn eid [& conditions]
+  (let [conditions (map #(concat ['?c] %) conditions)]
+    (->
+     {:find ['?c]
+      :where conditions}
+     q
+     ffirst)))
+
+(defn one [& conditions]
+  (if-let [id (apply eid conditions)]
+    (ent {:db/id id})))
 
 (defn prepare! []
   (let [schema-tx (read-string (slurp "resources/schema/remem-schema.edn"))
@@ -17,3 +37,5 @@
     ;; Since we're in dev, load the schema and seed data every time:
     @(d/transact connection schema-tx)
     @(d/transact connection data-tx)))
+
+(def t #(d/transact connection %))
--- src/clj/remembrance/models/article.clj
+++ src/clj/remembrance/models/article.clj
@@ -3,15 +3,25 @@
            [datomic.api :as d]
            [taoensso.timbre :refer [info]]))
 
-(defn show-article []
-  {})
+
+(defn show-article [guid]
+  (d/entity (db/db) (ffirst (d/q '[:find ?eid :in $ ?guid :where [?eid :article/guid ?guid]] (db/db) guid))))
 
 (defn create-article [attrs]
-  ;; (d/transact db/connection
-  ;;             [["db/add" (d/tempid "db.part/article")  ]]))
-  nil)
+  ;; try to find an existing one first
+  (let [original-url {attrs :original_url}
+        existing (d/q '[:find ?e :in $ ?original_url :where [?e :article/original_url ?original_url]] (db/db) original-url)]
+    (if (empty? existing)
+      (let [guid (str (d/squuid))]
+
+        ;; create the new article:
+        @(db/t [{:db/id (d/tempid "db.part/user")
+                 :article/guid guid
+                 :article/original_url original-url}])
+        ;; redirect to article
+        guid)
+      ((first existing) :guid))))
 
 (defn all-articles []
-  (let [db (d/db db/connection)
-        result-ids (d/q '[:find ?a :where [?a :article/original_url]] db)]
-    (map #(d/entity db (first %)) result-ids)))
+  (let [result-ids (d/q '[:find ?a :where [?a :article/guid]] (db/db))]
+    (map (fn [result-id] (d/entity (db/db) (first result-id))) result-ids)))
