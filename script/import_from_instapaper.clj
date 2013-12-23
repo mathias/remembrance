a95e4bc2aa2b0a1ebd75657f05b1df4c1820eab1
Matt Gauger <matt.gauger@gmail.com>
2013-12-23 12:41:37 -0600

    Refactor import from Instapaper script

--- script/import_from_instapaper.clj
+++ script/import_from_instapaper.clj
@@ -11,17 +11,30 @@
   (article-extract-text guid)
   (println "Ingested: " (:article/title (find-one-article-by-guid guid))))
 
+(defn ingest-and-set-state [article attrs]
+  ;; Always try to update the read status
+  (if (= "Archive" (:folder attrs))
+    (set-article-read-status article true))
+  (if-not (= :article.ingest_state/ingested (:article/ingest_state article))
+    (ingest-article (:article/guid article))
+    (println "Already have:" (:article/original_url article))))
+
+(defn create-and-ingest-article [attrs]
+  (if-let [article (create-article attrs)]
+    (ingest-and-set-state article attrs)
+    (println "Error creating article:"(:original_url attrs))))
+
+(defn valid-row? [attrs]
+  (not (nil? (:original_url attrs))))
+
 (defn ingest-instapaper-csv [filename]
    (with-open [rdr (io/reader filename)]
-     (doseq [line (line-seq rdr)]
-       (let [attrs (parse-row line)
-             article (create-article attrs)]
-         ;; Always try to update the read status
-         (if (= "Archive" (:folder attrs))
-           (set-article-read-status article true))
-         (if-not (= "ingested" (:article/ingest_state article))
-           (ingest-article (:article/guid article))
-           (println "Already have" (:original_url attrs)))))))
+     (doseq [line (rest (line-seq rdr))] ;; we get "rest" to ignore the column names on the first
+       (let [attrs (parse-row line)]
+         (if (valid-row? attrs)
+           (create-and-ingest-article attrs)
+           (println "Error. Not a valid record:" (or (:title attrs)
+                                                     (:original_url attrs))))))))
 
 (do
   (def csv-file (second *command-line-args*))
