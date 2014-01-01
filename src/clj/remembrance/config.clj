a17293fadb593719e9bfbf5e3ed0947462aab674
Matt Gauger <matt.gauger@gmail.com>
2014-01-01 00:07:22 -0600

    Load config file only once

--- src/clj/remembrance/config.clj
+++ src/clj/remembrance/config.clj
@@ -1,8 +1,14 @@
 (ns remembrance.config
-  (require [clojure.edn :as edn]))
+  (:require [clojure.edn :as edn]))
+
+(def config (atom {}))
 
 (def config-file-path "resources/config.edn")
 
 (defn load! []
-  (edn/read-string
-   (slurp config-file-path)))
+  (if-not (empty? @config)
+    @config
+    (swap! config
+           (fn [_ config] config)
+           (edn/read-string
+            (slurp config-file-path)))))
