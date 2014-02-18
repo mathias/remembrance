cfc8cd46bd2eef15e273b196b38c84ce12741258
Matt Gauger <matt.gauger@gmail.com>
2014-02-17 19:15:52 -0600

    Extract schemas to a test support file

--- test/remembrance/acceptance_test.clj
+++ test/remembrance/acceptance_test.clj
@@ -3,6 +3,7 @@
             [ring.mock.request :refer :all]
             [clojure.data.json :as json]
             [schema.core :as s]
+            [remembrance.test-support.schemas :refer :all]
             [remembrance.core :refer :all]))
 
 (defn parse-redirect-location [response]
@@ -123,85 +124,6 @@
 
        (fact "GET /api/health"
              (get! "/api/health") => ok?))
-
-(def Uri
-  "A URI represented by a string"
-  s/Str)
-
-(def Guid
-  "A GUID for a resource"
-  s/Str)
-
-(def FullArticle
-  "JSON schema for a full article's content"
-  {:articles
-   [{:href Uri
-     :guid Guid
-     :title s/Str
-     :original_url Uri
-     :readable_body s/Str
-     :read s/Bool}]})
-
-(def ArticleInfo
-  "JSON schema for individual article"
-  {:href Uri
-   :guid Guid
-   :title s/Str
-   :original_url Uri
-   :read s/Bool})
-
-(def ArticleList
-  "JSON schema for article list"
-  {:articles [ArticleInfo]})
-
-(def ArticleStats
-  "Stats about articles"
-  {:total s/Int
-   :ingested s/Int
-   :fetched s/Int
-   :errored s/Int
-   :read s/Int})
-
-(def NoteInfo
-  "JSON schema for an individual note"
-  {:guid Guid
-   :title s/Str
-   :body s/Str
-   :href Uri
-   :articles [Guid]})
-
-(def NoteList
-  "JSON schema for notes list"
-  {:notes [NoteInfo]})
-
-(def NoteStats
-  "Stats about articles"
-   {:total s/Int})
-
-(def ApiStats
-  "Stats for the entire system"
-  {:stats
-   {:articles ArticleStats
-    :notes NoteStats}})
-
-(def Timestamp
-  "A timestamp -- String for now until it can be further validated"
-  s/Str)
-
-(def ApiHealth
-  "Health checks for system in dev"
-  {:health
-   {:timestamp Timestamp
-    :database-value s/Str
-    :pid s/Int
-    :process-uptime s/Int
-    :memory {:free s/Int
-             :total s/Int}
-    :os {:hostname s/Str}}})
-
-(defn schema-valid? [schema-type]
-        (fn [actual]
-         (s/validate schema-type actual)))
 
 (defn parsed-json-body [resp]
   (-> resp
---
+++ test/remembrance/test_support/schemas.clj
@@ -0,0 +1,81 @@
+(ns remembrance.test-support.schemas
+  (:require [schema.core :as s]))
+
+(def Uri
+  "A URI represented by a string"
+  s/Str)
+
+(def Guid
+  "A GUID for a resource"
+  s/Str)
+
+(def FullArticle
+  "JSON schema for a full article's content"
+  {:articles
+   [{:href Uri
+     :guid Guid
+     :title s/Str
+     :original_url Uri
+     :readable_body s/Str
+     :read s/Bool}]})
+
+(def ArticleInfo
+  "JSON schema for individual article"
+  {:href Uri
+   :guid Guid
+   :title s/Str
+   :original_url Uri
+   :read s/Bool})
+
+(def ArticleList
+  "JSON schema for article list"
+  {:articles [ArticleInfo]})
+
+(def ArticleStats
+  "Stats about articles"
+  {:total s/Int
+   :ingested s/Int
+   :fetched s/Int
+   :errored s/Int
+   :read s/Int})
+
+(def NoteInfo
+  "JSON schema for an individual note"
+  {:guid Guid
+   :title s/Str
+   :body s/Str
+   :href Uri
+   :articles [Guid]})
+
+(def NoteList
+  "JSON schema for notes list"
+  {:notes [NoteInfo]})
+
+(def NoteStats
+  "Stats about articles"
+   {:total s/Int})
+
+(def ApiStats
+  "Stats for the entire system"
+  {:stats
+   {:articles ArticleStats
+    :notes NoteStats}})
+
+(def Timestamp
+  "A timestamp -- String for now until it can be further validated"
+  s/Str)
+
+(def ApiHealth
+  "Health checks for system in dev"
+  {:health
+   {:timestamp Timestamp
+    :database-value s/Str
+    :pid s/Int
+    :process-uptime s/Int
+    :memory {:free s/Int
+             :total s/Int}
+    :os {:hostname s/Str}}})
+
+(defn schema-valid? [schema-type]
+        (fn [actual]
+         (s/validate schema-type actual)))
