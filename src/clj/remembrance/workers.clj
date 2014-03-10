4087544a9209878739f990d49012e8f4ce693e0a
Matt Gauger <matt.gauger@gmail.com>
2014-03-10 17:54:16 -0500

    Cleanup

--- src/clj/remembrance/workers.clj
+++
@@ -1,5 +0,0 @@
-(ns remembrance.workers
-  (:require [remembrance.config :refer [production-env?]]
-            [taoensso.timbre :refer [info]]))
-
-(defn enqueue-article-original-html [article-guid])
