17c906df5df635bebac2f286ac5bddb8a979d14e
Matt Gauger <matt.gauger@gmail.com>
2014-09-24 15:43:04 -0500

    Yet more cleanup

--- project.clj
+++ project.clj
@@ -13,7 +13,6 @@
                  [ring/ring-jetty-adapter "1.3.1"]
                  [ring/ring-json "0.3.1"]]
 
-  ;; clojure source code pathname
   :source-paths ["src/clj"]
 
   :plugins [;; cljsbuild plugin
@@ -22,13 +21,11 @@
             ;; ring plugin
             [lein-ring "0.8.11"]]
 
-  ;; datomic configuration
   :profiles {:dev
              {:dependencies [[midje "1.6.3"]
                              [ring-mock "0.1.5"]]
               :plugins [[lein-midje "3.1.3"]]}}
 
-  ;; ring tasks configuration
   :ring {:handler remembrance.core/remembrance-handler
          :init remembrance.core/remembrance-init
          :auto-refresh true
