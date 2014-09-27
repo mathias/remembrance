192f16b349cfec1db74ad65c35574d3d05fc31af
Matt Gauger <matt.gauger@gmail.com>
2014-09-26 22:41:31 -0500

    Simplify requires, rename

--- src/clj/remembrance/importers/instapaper.clj
+++ src/clj/remembrance/importers/instapaper.clj
@@ -1,18 +1,16 @@
 (ns remembrance.importers.instapaper
   (:require [remembrance.models.article :refer :all]
-            [clojure.java.io :as io]
             [clojure-csv.core :as csv]
-            [clojure.walk :as walk]
+            [clojure.walk :refer [keywordize-keys]]
             [clojure.string :as string]
-            [hearst.url-cleanup :refer [normalize-url]]
-            [clojure.core.async :refer [chan go <! >!]]))
+            [clojure.core.async :refer [chan go <! >!]]
+            [taoensso.timbre :refer [info]]))
 
 (def create-and-ingest-article-chan (chan))
 
-(defn parse-file [csv-data]
+(defn parse-csv [csv-data]
   (let [[headers & rows] (csv/parse-csv csv-data)
         lowercased-headers (map string/lower-case headers)]
-    (map #(->> % (zipmap lowercased-headers) walk/keywordize-keys) rows)))
-
+    (map #(->> % (zipmap lowercased-headers) keywordize-keys) rows)))
 
 
